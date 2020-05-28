8d31b7574955f741c0c45e59ec02a865417dde36
Pratik Chowdhury <pratikc@live.co.uk>
2020-05-28 11:11:02 +0530

    Created Quarkus Auth Project

---
+++ src/test/java/com/pc/firebase/NativeFirebaseAuthIT.java
@@ -0,0 +1,9 @@
+package com.pc.firebase;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFirebaseAuthIT extends FirebaseAuthTest {
+
+    // Execute the same tests but in native mode.
+}
