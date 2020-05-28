2f119145cd982e787bff75ffeff29e8f09c23ca6
Pratik Chowdhury <pratikc@live.co.uk>
2020-05-28 11:55:49 +0530

    Added Firebase Code

--- src/main/java/com/pc/firebase/FirebaseAuth.java
+++ src/main/java/com/pc/firebase/FirebaseAuth.java
@@ -1,16 +1,26 @@
 package com.pc.firebase;
 
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import io.smallrye.jwt.auth.principal.DefaultJWTCallerPrincipal;
 
-@Path("/hello")
+@Path("/firebase/")
 public class FirebaseAuth {
 
     @GET
+    @Path("")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @PermitAll
+    public String hello(@Context SecurityContext securityContext) {
+        final var user = (DefaultJWTCallerPrincipal) securityContext.getUserPrincipal();
+        if (user == null)
+            return "Hello Anonymous User";
+        final var email = user.getClaim("email");
+        return "hello " + email;
     }
 }
