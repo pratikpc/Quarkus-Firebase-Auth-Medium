8ba28856029d19c65e1bde65675a94fafa373c26
Pratik Chowdhury <pratikc@live.co.uk>
2020-05-28 12:06:14 +0530

    Added Claim Names URL

--- src/main/java/com/pc/firebase/FirebaseAuth.java
+++ src/main/java/com/pc/firebase/FirebaseAuth.java
@@ -1,5 +1,6 @@
 package com.pc.firebase;
 
+import java.util.Set;
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -22,5 +23,16 @@ public class FirebaseAuth {
             return "Hello Anonymous User";
         final var email = user.getClaim("email");
         return "hello " + email;
+    }
+
+    @GET
+    @Path("claim_names")
+    @Produces(MediaType.TEXT_PLAIN)
+    @PermitAll
+    public Set<String> claimNames(@Context SecurityContext securityContext) {
+        final var user = (DefaultJWTCallerPrincipal) securityContext.getUserPrincipal();
+        if (user == null)
+            return Set.of();
+        return user.getClaimNames();
     }
 }
